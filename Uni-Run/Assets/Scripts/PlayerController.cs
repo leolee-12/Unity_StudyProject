ceb883c4f4efb0868bd22a06ebb2b7305765e410
LEO <leo.lee.dec@gmail.com>
2026-08-28 00:23:59 +0900

    [feat] PlayerController 마무리 / 배경-발판 스크롤링 / TextMeshPro(TMP) 추가 및 UI 구현 - PlayerController : Trigger, Collision 메서드 구현(바닥 판정, 사망 처리 등) - 배경-발판 스크롤링 : 배경과 발판이 움직여 캐릭터가 달리는 느낌 주기 - TMP : 다양한 텍스트 표현 위해 TMP 도입(필수 에셋 설치), 점수 및 게임오버 UI 텍스트 추가

--- Uni-Run/Assets/Scripts/PlayerController.cs
+++ Uni-Run/Assets/Scripts/PlayerController.cs
@@ -55,14 +55,21 @@ public class PlayerController : MonoBehaviour {
    }
 
    private void OnTriggerEnter2D(Collider2D other) {
-       // 트리거 콜라이더를 가진 장애물과의 충돌을 감지
-   }
+        if (other.tag == "Dead" && !isDead)
+        {
+            Die();
+        }
+    }
 
    private void OnCollisionEnter2D(Collision2D collision) {
-       // 바닥에 닿았음을 감지하는 처리
-   }
+        if (collision.contacts[0].normal.y > 0.7f)
+        { // 어떤 콜라이더와 닿았으며, 충돌 표면이 위쪽을 보고 있으면 (y = 0.7f일 때 대략 45도 경사로 위를 향함)
+            isGrounded = true;
+            jumpCount = 0;
+        }
+    }
 
    private void OnCollisionExit2D(Collision2D collision) {
-       // 바닥에서 벗어났음을 감지하는 처리
-   }
+        isGrounded = false;
+    }
 }
