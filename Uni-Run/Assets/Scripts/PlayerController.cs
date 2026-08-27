b7e077894e631e8e7f76911e80905f2971d5da19
LEO <leo.lee.dec@gmail.com>
2026-08-27 21:52:54 +0900

    [feat] PlayerController 구현 중

--- Uni-Run/Assets/Scripts/PlayerController.cs
+++ Uni-Run/Assets/Scripts/PlayerController.cs
@@ -5,24 +5,53 @@ public class PlayerController : MonoBehaviour {
    public AudioClip deathClip; // 사망시 재생할 오디오 클립
    public float jumpForce = 700f; // 점프 힘
 
-   private int jumpCount = 0; // 누적 점프 횟수
+   private int jumpCount = 0; // 누적 점프 횟수(바닥에 닿으면 0으로 리셋)
    private bool isGrounded = false; // 바닥에 닿았는지 나타냄
    private bool isDead = false; // 사망 상태
 
-   private Rigidbody2D playerRigidbody; // 사용할 리지드바디 컴포넌트
-   private Animator animator; // 사용할 애니메이터 컴포넌트
-   private AudioSource playerAudio; // 사용할 오디오 소스 컴포넌트
+   private Rigidbody2D playerRigidbody;
+   private Animator animator;
+   private AudioSource playerAudio;
 
    private void Start() {
        // 초기화
+       playerRigidbody = GetComponent<Rigidbody2D>();
+       animator = GetComponent<Animator>();
+       playerAudio = GetComponent<AudioSource>();
    }
 
    private void Update() {
-       // 사용자 입력을 감지하고 점프하는 처리
+        if (isDead)
+        {
+            // 사망 시 갱신 작업 없이 종료
+
+            return;
+        }
+
+        if (Input.GetMouseButtonDown(0) && jumpCount < 2)
+        {   // 마우스 좌클릭 시 && 최대 점프 횟수 도달하지 않았을 때
+            jumpCount++;
+            playerRigidbody.linearVelocity = Vector2.zero;  // 점프 직전에 속도를 0으로 변경
+            playerRigidbody.AddForce(new Vector2(0, jumpForce));    // 위쪽으로 힘을 주기
+            playerAudio.Play();
+        }
+        else if (Input.GetMouseButtonUp(0) && playerRigidbody.linearVelocity.y > 0)
+        {   // 마우스 좌클릭을 떼는 순간 && 속도의 y값이 양수라면(위로 상승 중)
+            playerRigidbody.linearVelocity *= 0.5f; // 속도를 절반으로
+        }
+
+        animator.SetBool("Grounded", isGrounded);
    }
 
    private void Die() {
-       // 사망 처리
+        animator.SetTrigger("Die");
+
+        playerAudio.clip = deathClip;
+        playerAudio.Play();
+
+        playerRigidbody.linearVelocity = Vector2.zero;
+        
+        isDead = true;
    }
 
    private void OnTriggerEnter2D(Collider2D other) {
